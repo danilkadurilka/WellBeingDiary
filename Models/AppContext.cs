d927e35ae4c626a9b4828a178a5e3d2acd06fcce
danilka.kurs2007@gmail.com <danilka.kurs2007@gmail.com>
2025-12-08 12:03:28 +0700

    #

--- Models/AppContext.cs
+++ Models/AppContext.cs
@@ -14,7 +14,6 @@ namespace WellBeingDiary.Models
         public DbSet<Medicine> Medicines { get; set; }
         public DbSet<Report> Reports { get; set; }
         public DbSet<Symptom> Symptoms { get; set; }
-        public DbSet<DailySymptom> DailySymptoms { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=WellBeingDiary.db");
